1473ca6bd93c0a3335ae1b2cad8733f894a0aa54
Robert Beachill <bbeachill@icloud.com>
2025-06-08 22:19:40 +0100

    Initial Commit

---
+++ CatFacts/CatFactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatFactsApp.swift
+//  CatFacts
+//
+//  Created by Robert Beachill on 08/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct CatFactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
